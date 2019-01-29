fe8715613732a1c2d789456dc58d64799af68344
jonathan450 <44813750+jonathan450@users.noreply.github.com>
2019-01-29 10:22:48 -0700

    Add files via upload

---
+++ hangman003.py
@@ -0,0 +1,20 @@
+word='hangman'
+secret_word=list(word)
+print(secret_word)
+new_word= ['*' for i in range(len(secret_word))]
+print(new_word)
+penalty=0
+letters_used=[]
+while'*' in new_word and penalty<7:
+    guess=input('Enter a letter \n')
+    if guess not in secret_word or guess in letters_used:
+        penalty += 1
+    else:
+        for i in range(len(secret_word)):
+            if guess == secret_word[i]:
+                new_word[i]=secret_word[i]
+    letters_used.append(guess)
+    print('Penalty', penalty)
+    print(letters_used)
+    print(new_word)
+print('The End')
